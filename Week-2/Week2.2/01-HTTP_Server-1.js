6bda22fe40b5a8ffb8f7bfa517d4280e139944ae
Saurav Kumar <saurav79911@gmail.com>
2026-07-02 00:25:47 +0530

    Added Express basics

---
+++ Week-2/Week2.2/01-HTTP_Server-1.js
@@ -0,0 +1,13 @@
+// Create a HTTP server , using library ( express)
+
+const express = require("express");
+
+const port = 3000;
+const app  = express();
+
+
+app.get('/' , function(req , res) {
+    res.send('Hello World')
+})
+
+app.listen(port);
