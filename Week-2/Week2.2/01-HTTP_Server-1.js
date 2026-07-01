010bdf63ceaa6e7946c1877e704e84c0a0fb6a19
Saurav Kumar <saurav79911@gmail.com>
2026-07-02 00:52:15 +0530

    Added Express basics

--- Week-2/Week2.2/01-HTTP_Server-1.js
+++ Week-2/Week2.2/01-HTTP_Server-1.js
@@ -6,8 +6,18 @@ const port = 3000;
 const app  = express();
 
 
+app.get('/route-handler' , function( req , res) {
+    // header , body , query parameters
+    // do machine learnig model
+    res.json({
+        name: "saurav" ,
+        age : 21
+    })
+})
 app.get('/' , function(req , res) {
     res.send('Hello World')
 })
 
-app.listen(port);
+app.listen(port , function() {
+    console.log("listening from  port 3000")
+});
