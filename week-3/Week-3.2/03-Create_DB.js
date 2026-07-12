d885af6f472935af319f03e609ff6c6d242a2706
Saurav Kumar <saurav79911@gmail.com>
2026-07-13 02:21:33 +0530

    DB created

--- week-3/Week-3.2/03-Create_DB.js
+++ week-3/Week-3.2/03-Create_DB.js
@@ -1,16 +1,42 @@
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+require("dotenv").config({
+  path: __dirname + "/../../.env",
+});
+// require("dotenv").config();
+
 const mongoose = require("mongoose");
-require("dotenv").config();
-mongoose.connect(process.env.MONGO_URI + "user_appNew");
+
+const MONGO_URL = process.env.MONGO_URL;
 
 const User = mongoose.model("Users", {
   name: String,
   email: String,
   password: String,
 });
-const user = new User({
-  name: "saurav",
-  email: "saurav@gmail.com",
-  password: "123",
-});
 
-user.save();
+console.log("Connecting to MongoDB...");
+
+mongoose
+  .connect(MONGO_URL + "user_app")
+  .then(async () => {
+    console.log("DB Connected successfully! 🎉");
+
+    const user = new User({
+      name: "saurav",
+      email: "saurav@gmail.com",
+      password: "123",
+    });
+
+    const doc = await user.save();
+
+    console.log("Data successfully saved! 🎉");
+    console.log(doc);
+
+    await mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.error("Error hai:", err);
+  });
